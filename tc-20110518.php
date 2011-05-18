b19deb6a8925e8cf6b75a81ba54e07a2da178da8
yitznewton <yitzchak.schaffer@gmx.com>
2011-05-18 17:38:40 -0400

    basic functional tc templates

---
+++ tc-20110518.php
@@ -0,0 +1,131 @@
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+"http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="shortcut icon" href="/sites/all/themes/tc1/images/favicon.ico" type="image/x-icon" />
+  <title><?php echo has_slot('title') ? get_slot('title') . ' | ' : '' ?>Touro College Libraries</title>
+
+  <meta name="title" content="Touro College Libraries">
+  <meta name="description" content="Website of the Touro College Libraries">
+  <meta name="subject" content="Touro College Libraries, Touro Libraries, Touro College Library,
+    Touro Library, TCL, Turo Library, Turo Libraries, SCAS, DMX">
+
+  <?php include_stylesheets() ?>
+  <?php include_javascripts() ?>
+<script type="text/javascript">
+<!--//--><![CDATA[//><!--
+jQuery.extend(Drupal.settings, { "basePath": "/", "googleanalytics": { "trackOutgoing": 1, "trackMailto": 1, "trackDownload": 1, "trackDownloadExtensions": "7z|aac|avi|csv|doc|exe|flv|gif|gz|jpe?g|js|mp(3|4|e?g)|mov|pdf|phps|png|ppt|rar|sit|tar|torrent|txt|wma|wmv|xls|xml|zip" } });
+//--><!]]>
+</script>
+</head>
+
+<body>
+
+
+
+<div id="logo-header">
+  <a href="http://www.touro.edu/"><img id="screen-logo" src="http://www.tourolib.org/sites/all/themes/tc1/images/touro.jpg"
+    alt="Touro College logo" /></a>
+  <img id="print-logo" src="http://www.tourolib.org/sites/all/themes/tc1/images/touro_libraries.jpg" alt="Touro College Libraries logo" />
+</div>
+
+
+<div id="touro-menubar-container">
+  <div id="block-block-4" class="block block-block">
+
+  <div class="content">
+    <ul id="touro-menubar"><li><h3>Home</h3><ul><li><a href="http://www.touro.edu/">Touro Home Page</a></li><li><a href="http://www.touro.edu/general/about.asp">About Touro College</a></li><li><a href="http://www.touro.edu/general/mission.asp">Mission Statement</a></li><li><a href="http://www.touro.edu/general/pres.asp">President's Office</a></li><li><a href="http://www.touro.edu/media/">News Center</a></li><li><a href="http://www.touro.edu/general/jobopenings/">Careers @ Touro</a></li></ul></li><li><h3>Academics</h3><ul><li><a href="http://www.touro.edu/general/undergraduate.asp">Undergraduate</a></li><li><a href="http://www.touro.edu/general/graduate.asp">Graduate</a></li><li><a href="http://www.touro.edu/general/healthsciences.asp">Health Sciences</a></li><li><a href="http://www.touro.edu/general/intlprograms.asp">International Programs</a></li><li><a href="http://www.touro.edu/general/onlineprograms.asp">Online Programs</a></li><li class="last-in-section"><a href="http://www.touro.edu/general/specialprograms.asp">Special Programs</a></li><li><a href="http://www.touro.edu/assessment/">Assessment and Evaluation</a></li><li><a href="http://www.touro.edu/general/bulletins.asp">College Bulletins</a></li><li><a href="http://www.touro.edu/general/departments.asp">Department Web Sites</a></li></ul></li><li><h3>Libraries</h3><ul><li><a href="http://www.touro.edu/library/">Library Home</a></li><li><a href="http://www.touro.edu/library/directory/">Library Directory</a></li><li><a href="http://facpubs.tourolib.org/">Faculty Publications</a></li><li><a href="http://www.tourolib.org/archives/">Archives</a></li></ul></li><li><h3>Student&nbsp;Services</h3><ul><li><a href="http://www.touro.edu/calendars/">Academic Calendars</a></li><li><a href="http://www.touro.edu/advisement/">Advisement & Counseling</a></li><li><a href="http://www1.touro.edu/comm/">Commencement</a></li><li><a href="http://www.touro.edu/general/computerlabhours.asp">Computer Labs</a></li><li><a href="http://www.touro.edu/lrc/">Learning Resource Centers</a></li><li><a href="http://www.touro.edu/general/studentorgs.asp">Student Organizations</a></li><li class="last-in-section"><a href="http://www.touro.edu/general/teacherhowto.asp">Teacher Certification</a></li><li><a href="http://www.touro.edu/registrar/">Office of the Registrar</a></li><li><a href="http://www.touro.edu/bursar/">Office of the Bursar</a></li><li><a href="http://tcweb.touro.edu/">TCWeb - Web-based Student Services</a></li><li><a href="http://www.touro.edu/general/transcripts.asp">Transcript Requests</a></li><li><a href="http://www.touro.edu/general/studentservices_voterRegistration.asp">Voter Registration</a></li></ul></li><li><h3>Giving</h3><ul><li><a href="http://www.touro.edu/giving/">Giving @ Touro</a></li></ul></li><li><h3>Research</h3><ul><li><a href="http://www.touro.edu/general/osp.asp">Office of Sponsored Programs</a></li><li><a href="http://www1.touro.edu/shs/home/index.php?disp=irb">The Health Sciences Institutional Review Board (IRB)</a></li><li><a href="http://www.touro.edu/irb/">The Institutional Review Board #1 (IRB #1)</a></li></ul></li><li><h3>Directory</h3><ul><li><a href="http://www.touro.edu/general/locationsnyc.asp">New York Locations</a></li><li><a href="http://www.touro.edu/general/locationsworld.asp">World-Wide Locations</a></li><li><a href="http://www.touro.edu/general/directory/findex.asp">Faculty Directory</a></li><li><a href="http://www.touro.edu/general/directory/">Staff Directory</a></li></ul></li><li><h3>Web&nbsp;Courses</h3><ul><li><a href="http://www.touro.edu/onlineCourses/">Online Courses - NY/Israel</a></li><li><a href="http://blackboard.touro.edu/">Blackboard</a></li></ul></li><li><h3>E-Mail</h3><ul><li><a href="https://owa.touro.edu">Outlook Web Access</a></li><li><a href="http://www.touro.edu/general/email.asp">E-mail Help</a></li></ul></li><li><h3>News</h3><ul><li><a href="http://www.touro.edu/media/">News Center</a></li></ul></li></ul>  </div>
+</div>
+  <a id="touro-menubar-text-link"
+    href="http://www.touro.edu/nav/current/textmenu.asp">Click here for text menu (or enable Javascript for regular menu).</a>
+</div>
+
+<div class="clear"></div>
+
+<div id="ie-main-container"><div id="main-container">
+  <div id="sidebar">
+    <h2 id="sidebar-department"><a href="/">Libraries</a></h2>
+
+      <ul id="sidebar-links">
+    <li class="first"><a href="/" title="">Library Home</a></li><li><a href="http://library.touro.edu/search~S2" title="">Find Books and More (Catalog)</a></li><li><a href="https://erms.tourolib.org/" title="">Find Articles and Databases</a></li><li><a href="/resources/ereserves" title="">EReserves</a></li><li><a href="/services/students" title="">Student Services</a></li><li class="last"><a href="/services/faculty" title="">Faculty Services</a></li><li><a href="/newsletter" title="">Newsletter</a></li>  </ul>
+
+    <h2>Search</h2>
+    <div id="sidebar-search">
+      <form id="searchbox_018385097459874495184:9goqy74m_fi"
+        action="http://www.google.com/search">
+
+        <input class="input-text" id="search-q" name="q" type="text" />
+        <input type="hidden" name="cx" value="018385097459874495184:9goqy74m_fi" />
+        <input type="hidden" name="cof" value="FORID:0" />
+
+        <img id="search-logo" src="http://www.google.com/coop/images/google_custom_search_smnar.gif"
+          alt="Google Custom Search" />
+
+        <input class="input-button" id="search-sa" type="submit" name="sa" value="Search" />
+      </form>
+    </div>
+
+    <ul id="social-networking">
+      <li>
+        <a href="http://www.facebook.com/tourolibraries">
+          <img src="http://www.tourolib.org/sites/all/themes/tc1/images/facebookb.png"
+            alt="Facebook logo" title="Like us on Facebook" /></a>
+      </li>
+
+      <li>
+        <a href="http://twitter.com/TouroLibraries">
+          <img src="http://www.tourolib.org/sites/all/themes/tc1/images/twitterb.png"
+            alt="Twitter logo" title="Follow us on Twitter" /></a>
+        <a id="twitter-link" href=""></a>
+      </li>
+    </ul>
+
+  </div>
+
+
+  <div id="top-links">
+<div id="block-menu-menu-top-links" class="block block-menu">
+
+  <div class="content">
+    <ul class="menu"><li class="leaf first"><a href="/about/libraries" title="">Locations and Hours</a></li>
+<li class="leaf"><a href="/account" title="">My Account</a></li>
+<li class="leaf"><a href="http://library.touro.edu/patroninfo~S2" title="">Renew Books</a></li>
+<li class="leaf last"><a href="/faq" title="">FAQ</a></li>
+</ul>  </div>
+</div>
+  </div>
+
+  <div id="content">
+
+    <?php echo $sf_content ?>
+
+  </div><!-- end of #content -->
+
+  <div class="clear"></div>
+
+</div></div><!-- end of #main-container and #ie-main-container -->
+
+<div id="footer">
+    <ul id="footer-links" class="inline-menu">
+    <li class="first"><a href="/tech-support" title="">Technical Support</a></li><li><a href="/contact" title="">Contact Us</a></li><li><a href="http://www.touro.edu/general/disclaimer.asp" title="">Disclaimer</a></li><li><a href="http://www.touro.edu/docs/TouroCollege-WebsitePrivacyPolicy-Final(110509).pdf" title="">Privacy Policy</a></li><li><a href="http://www.touro.edu/docs/TouroCollege-WebsiteTermsofUse-Final(110509).pdf" title="">Terms of Use</a></li><li class="last"><a href="https://wiki.tourolib.org/" title="">Staff Wiki</a></li><li><a href="/feeds" title="" class="rss-link">RSS</a></li>  </ul>
+  <div>Copyright &copy; 2011 Touro College. All rights reserved.</div>
+<div>Touro College Main Campus &ndash; 27-33 West 23 Street, New York, NY 10010, (212) 463-0400</div></div>
+
+<script type="text/javascript" src="/sites/default/files/js/js_87361fd17d9efcf9815ba8604e6962c5.js"></script>
+<script type="text/javascript">
+<!--//--><![CDATA[//><!--
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+//--><!]]>
+</script>
+<script type="text/javascript">
+<!--//--><![CDATA[//><!--
+try{var pageTracker = _gat._getTracker("UA-11909015-1");pageTracker._trackPageview();} catch(err) {}
+//--><!]]>
+</script>
+
+</body>
+</html>
